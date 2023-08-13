5c4437ef8b95eed3876b3a1ee78a16dc3fdda6ce
Kat Tschan <kat@kattschan.xyz>
2023-08-12 23:02:53 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
             name: "SwiftVue",
             targets: ["SwiftVue"]),
     ],
-    version: "1.0.0",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
